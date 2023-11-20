542f373f83304a94e3bc90b26287a800013cd53e
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-11-20 08:36:11 +0100

    Done: Node native Modules: Process and ls examples

---
+++ 2-native-modules/fs/2.example-ls-advanced.js
@@ -0,0 +1,36 @@
+const fs = require('node:fs/promises')
+const path = require('node:path')
+
+const folder = process.argv[2] ?? '.'
+
+async function ls(directory){
+    let files
+    try{
+        files = await fs.readdir(directory)
+    } catch {
+        console.log(`No se pudo leer el directorio ${directory}`)
+        process.exit(1)
+    }
+
+    const filesPromises = files.map(async file => {
+        const filePath = path.join(directory,file)
+        let fileStats
+        try {
+            fileStats = await fs.stat(filePath) // Informacion del archivo
+        } catch {
+            console.log(`No se puede leer el archivo ${filePath}`)
+            process.exit(1)
+        }
+        const isDirectory = fileStats.isDirectory()
+        const fileType = isDirectory ? 'd' : '-'
+        const fileSize = fileStats.size
+        const fileModified = fileStats.mtime.toLocaleString()
+
+        return `${fileType} ${file.padEnd(40)} ${fileSize.toString().padStart(10)} ${fileModified}`
+    })
+    
+    const filesInfo = await Promise.all(filesPromises)
+    filesInfo.forEach(fileInfo=>{console.log(fileInfo)})
+}
+
+ls(folder)
---
+++ 2-native-modules/process/4.process.js
@@ -0,0 +1,17 @@
+//Argumentos de entrada
+console.log(process.argv)
+
+//Current working directory
+console.log('CWD', process.cwd())
+
+//Controlar el proceso y su salida
+
+//platform
+console.log(process.env.OS)
+
+process.exit(1)
+
+//Controlar eventos del proceso
+process.on('exit', ()=>{
+    // Limpiar los recursos
+})
