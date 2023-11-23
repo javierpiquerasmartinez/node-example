faa8ab00d08f5780f4206069831f60683f95660e
Javier Piqueras <javierpiquerasmartinez@gmail.com>
2023-11-23 16:25:21 +0100

    WIP | express example

--- 3-express/3. express.js
+++ 3-express/3. express.js
@@ -1,31 +1,49 @@
 const express = require('express')
+const ditto = require('./pokemon/ditto.json')
+const path = require('path')
+
+const PORT = process.env.PORT ?? 1234
 
 const app = express()
 app.disable('x-powered-by')
 
-const port = process.env.PORT ?? '3000'
-
-app.use((req, res, next) => {
-  console.log('Middleware')
-  next()
-})
-
-// Middleware that puts all chunks into the body
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.statusCode = 200
-  res.send('Hello World')
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   // solo llegan request que son POST y que tienen el header Content-Type: application/json
+//   let body = ''
+
+//   // escuchar el evento data
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     // mutar la request y meter la información en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
+
+app.get('/pokemon/ditto', (req, res) => {
+  res.json(ditto)
 })
 
-app.post('/pokemon/seviper', (req, res) => {
-  res.status(201).json({ pokemon: 'seviper' })
+app.post('/pokemon', (req, res) => {
+  // req.body deberíamos guardar en bbdd
+  res.status(201).json(req.body)
 })
 
+// la última a la que va a llegar
 app.use((req, res) => {
-  res.status(404).send('Not found')
+  res.status(404).send('<h1>404</h1>')
 })
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
+app.listen(PORT, () => {
+  console.log(`server listening on port http://localhost:${PORT}`)
 })
